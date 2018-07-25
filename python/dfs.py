16a658ab6ab42ec91d36262dff3af85f8cc77f36
Aurora Nou <auroranou@gmail.com>
2018-07-25 17:41:27 -0400

    Implement recursive DFS
    Co-authored-by: Lydia Ding <lydiding@gmail.com>

--- python/dfs.py
+++ python/dfs.py
@@ -14,14 +14,33 @@ def dfs(graph, start):
 
     return traversed
 
+def dfs_recursive(graph, curr):
+    traversed = []
+    def dfs_helper(curr):
+        if not curr.traversed:
+            curr.traversed = True
+            traversed.append(curr)
+            for neighbor in curr.neighbors:
+                dfs_helper(neighbor)
+    dfs_helper(curr)
+    return traversed
+
 class TestDFS(unittest.TestCase):
     def test_one_edge(self):
         v1 = Vertex("me")
         v2 = Vertex("you")
         g = Graph({v1, v2})
         g.add_edge(v1, v2)
-
         path = dfs(g, v1)
+        self.assertTrue(path in [
+            [v1, v2]
+        ])
+
+        v1 = Vertex("me")
+        v2 = Vertex("you")
+        g = Graph({v1, v2})
+        g.add_edge(v1, v2)
+        path = dfs_recursive(g, v1)
         self.assertTrue(path in [
             [v1, v2]
         ])
@@ -40,6 +59,21 @@ class TestDFS(unittest.TestCase):
             [a, c, d, b],
             [a, d, c, b]
         ])
+
+        d = Vertex("d")
+        c = Vertex("c", {d})
+        b = Vertex("b", {c})
+        a = Vertex("a", {b, c, d})
+        g = Graph({a, b, c, d})
+
+        path = dfs_recursive(g, a)
+        self.assertTrue(path in [
+            [a, b, c, d],
+            [a, d, b, c],
+            [a, c, d, b],
+            [a, d, c, b]
+        ])
+
 
 if __name__ == "__main__":
     unittest.main()
