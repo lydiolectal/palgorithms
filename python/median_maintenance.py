c866cc132e0298ec2e95614501af6cfde0a7b250
lydiolectal <lydiding@gmail.com>
2018-08-30 11:19:15 -0400

    implement median maintenance usingh heaps

--- python/median_maintenance.py
+++ python/median_maintenance.py
@@ -8,7 +8,7 @@ def get_medians(l):
     for num in l:
         median = insert(num, l, low_heap, high_heap, cur_median)
         medians.append(median)
-        cur_median = low_heap[0]
+        cur_median = -low_heap[0]
     return medians
 
 def insert(num, l, low_heap, high_heap, cur_median):
@@ -23,8 +23,8 @@ def insert(num, l, low_heap, high_heap, cur_median):
         if num < cur_median:
             heapq.heappush(low_heap, -num)
         else:
+            heapq.heappush(high_heap, num)
             temp = heapq.heappop(high_heap)
-            heapq.heappush(high_heap, -num)
             heapq.heappush(low_heap, -temp)
     median = -low_heap[0]
     return median
@@ -36,13 +36,19 @@ class TestMedian(unittest.TestCase):
         self.assertEqual(get_medians(l), [1, 1, 2])
 
     def test_three(self):
-        l = [1, 2, 3, 4]
-        self.assertEqual(get_medians(l), [1, 1, 2, 2])
+        l = [4, 1, 3, 2]
+        self.assertEqual(get_medians(l), [4, 1, 3, 2])
+
+    def test_fifteen(self):
+        l = [4, 5, 6, 14, 9, 11, 2, 12, 7, 0, 10, 8, 13, 3, 1]
+        expected = [4, 4, 5, 5, 6, 6, 6, 6, 7, 6, 7, 7, 8, 7, 8]
 
 if __name__ == "__main__":
 
-     # with open("median_maintenance.txt") as f:
-     #     l = [int(i) for i in f.read().splitlines()]
-     #     print(str(l[0]))
-
     unittest.main()
+
+    l = []
+    with open("median_maintenance.txt") as f:
+         l = [int(i) for i in f.read().splitlines()]
+         medians = get_medians(l)
+    print(sum(medians))
